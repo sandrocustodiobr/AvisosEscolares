1e040b0a00db256929945c32909834f83878011a
Sandro Custodio <sandrocustodio@gmail.com>
2018-04-03 12:54:10 -0300

    Mudar Senha concluido e senha com criptografia.

--- Responsaveis/responsaveis/responsaveis-upd.php
+++ Responsaveis/responsaveis/responsaveis-upd.php
@@ -28,20 +28,19 @@ include_once '../sistema/topo.php';
 
 <form method="POST" action="responsaveis-upd-ok.php">
     <div id="formulario">
-        <label>ID:</label>
-        <input class="form-control" name="id" type="number" value="<?php echo $item->id; ?>" readonly/><br>
+        <input name="id" type="number" value="<?php echo $item->id; ?>" readonly hidden/><br>
 
         <label>Nome:</label>
         <input class="form-control" name="nome" type="text" value="<?php echo $item->nome; ?>" maxlength="50"/><br>
 
-        <label>Admin:</label>
-        <input class="form-inline" name="admin" type="radio" value="1" <?php if ($item->admin) { echo "checked"; } ?>/> Sim
-        <input class="form-inline" name="admin" type="radio" value="0" <?php if (!$item->admin) { echo "checked"; } ?>/> Não<br>
-
         <label>Email:</label>
         <input class="form-control" name="email" type="email" value="<?php echo $item->email; ?>" maxlength="50"/><br>
 
-        <button type="submit" name="botao" value="Enviar" class="btn btn-success">Enviar</button> &nbsp;&nbsp;
+        <label>Admin:</label> &nbsp;&nbsp;&nbsp;&nbsp;
+        <input class="form-inline" name="admin" type="radio" value="1" <?php if ($item->admin) { echo "checked"; } ?>/> Sim &nbsp;&nbsp;&nbsp;&nbsp;
+        <input class="form-inline" name="admin" type="radio" value="0" <?php if (!$item->admin) { echo "checked"; } ?>/> Não<br><br>
+        
+        <button type="submit" name="botao" value="Enviar" class="btn btn-success">Enviar</button> &nbsp;&nbsp;        
         <button type="reset" class="btn btn-danger">Limpar</button> &nbsp;&nbsp;
         <a href='responsaveis-listar.php' class='btn btn-default'>Voltar</a>
     </div>
